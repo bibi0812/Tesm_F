fa1c179b4ae8044f35e268a6ca5ea4feebdfba5d
DESKTOP-4BMUEVD\GA1B <h1202519016@hiratagakuen.onmicrosoft.com>
2025-11-10 12:11:13 +0900

    ドラゴン２

--- Team_F/Assets/matuda eiji/Brake.cs
+++ Team_F/Assets/matuda eiji/Brake.cs
@@ -4,11 +4,7 @@ public class BreakBlock : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-<<<<<<< HEAD
-        // 衝突相手が「Ball」タグを持っているかチェック（スペルミスを修正）
-=======
-        // ボール（または弾など）に当たったら非表示にする
->>>>>>> 49b93ae4b242368dd87aee5da42aa5d3afe573b9
+
         if (collision.gameObject.CompareTag("Player"))
         {
             gameObject.SetActive(false);
