ee3f7eadd7683f6d4e176a8a331666c3c50b600a
kouta kusumoto <h1202515023@hiratagakuen.onmicrosoft.com>
2025-11-28 12:17:17 +0900

    a

---
+++ Team_F/Assets/morimoto asuto/Enemy/FireBreathHit.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class FireBreathHit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        // Åö ï«Ç…ìñÇΩÇ¡ÇΩÇÁè¡Ç¶ÇÈ
+        if (collision.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
