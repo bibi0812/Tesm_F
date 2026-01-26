2ac591da3cda48c2b90629ef40554beb938043cb
matudaeiji <h1202519013@hiratagakuen.onmicrosoft.com>
2026-01-26 13:55:30 +0900

    script整理

--- Team_F/Assets/matuda eiji/BulletSpawner.cs
+++ Team_F/Assets/matuda eiji/BulletSpawner.cs
@@ -8,24 +8,6 @@ public class BulletSpawner : MonoBehaviour
     public float spawnInterval = 0.5f; // 弾を出す間隔
     private float timer = 0f;          // 経過時間
 
-    //void Awake()
-    //{
-    //    // シングルトンの設定
-    //    if (instance == null)
-    //    {
-    //        instance = this;
-
-    //        // シーンをまたいでも削除しない
-    //        DontDestroyOnLoad(gameObject);
-    //    }
-    //    else
-    //    {
-    //        // 2個目以降は削除 → 弾が倍増するのを防ぐ
-    //        Destroy(gameObject);
-    //        return;
-    //    }
-    //}
-
     void Update()
     {
         // 時間を計測
